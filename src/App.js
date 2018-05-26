ea4a24cb682641b60c8adaf25ad78a1f4e05548f
Gennaro <g.anzalone12.com>
2018-05-26 15:07:42 -0700

    lsson

--- src/App.js
+++ src/App.js
@@ -3,38 +3,46 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: 'G', age: 28 },
-      { name: 'Gwen', age: 28 },
-      { name: 'Nick', age: 28 }
-    ],
-    otherState: 'Some Other Value'
-  }
-
-  switchNameHandler = (newName) => {
-    // console.log('Was Clicked');
-    // DON'T DO THIS: this.state.persons[0].name = 'Gennaro';
-    this.setState({
+    state = {
       persons: [
-        { name: newName, age: 28 },
-        { name: 'Gwen', age: 28 },
-        { name: 'Nick', age: 32 }
-      ] 
-    })
+        { id: 'smh', name: 'G', age: 28 },
+        { id: 'wow', name: 'Gwen', age: 28 },
+        { id: 'lol', name: 'Nick', age: 32 }
+      ],
+      otherState: 'Some Other Value',
+        showPersons: false
+    }
+
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState( {persons: persons} );
+
+    }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'G', age: 28 },
-        { name: event.target.value, age: 28 },
-        { name: 'Nick', age: 32 }
-      ]
-    } )
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
-  render() {
+  render () {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
@@ -43,23 +51,31 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-       <h1>Hi, I'm a React App!</h1>
-       <p>This is really working!</p>
-       <button 
-       style={style}
-       onClick={() => this.switchNameHandler('Gennaro!!')}>Switch Name</button>
-       <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-       <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Gee!')} changed={this.nameChangedHandler}>My Hobbies: Yoga</Person>
-       <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+         <h1>Hi, I'm a React App!</h1>
+          <p>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
       </div>
     );
   }
